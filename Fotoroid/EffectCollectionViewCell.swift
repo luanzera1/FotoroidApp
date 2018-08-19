f8d4fe2a1116612a4a7791bad2c35ed35cf56fbc
luanzera1 <luanz@live.com>
2018-08-19 20:42:41 -0300

    Initial Commit

---
+++ Fotoroid/EffectCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EffectCollectionViewCell.swift
+//  Fotoroid
+//
+//  Created by luanzera1 on 19/08/2018.
+//  Copyright © 2018 Luan Souza. All rights reserved.
+//
+
+import UIKit
+
+class EffectCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ivEffect: UIImageView!
+}
